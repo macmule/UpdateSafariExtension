cc73235e52f91677f2dc5b15e702a46541964202
macmule <bentoms@btopenworld.com>
2015-09-25 21:58:09 +0100

    Update UpdateSafariExtension.sh

--- UpdateSafariExtension.sh
+++ UpdateSafariExtension.sh
@@ -16,22 +16,11 @@
 ####################################################################################################
 
 # HARDCODED VALUE FOR "PATHTOEXTENSION" IS SET HERE
-pathToExtension=""
 extensionRegex=""
+pathToExtension=""
 
-# CHECK TO SEE IF A VALUE WAS PASSED IN PARAMETER 4 AND, IF SO, ASSIGN TO "PATHTOEXTENSION"
-if [ "$4" != "" ] && [ "$pathToExtension" == "" ];then
-    pathToExtension=$4
-fi
-
-# Error if variable appName is empty
-if [ "$pathToExtension" == "" ]; then
-	echo "Error:  No value was specified for the pathToExtension variable..."
-	exit 1
-fi
-
-# CHECK TO SEE IF A VALUE WAS PASSED IN PARAMETER 5 AND, IF SO, ASSIGN TO "extensionRegex"
-if [ "$5" != "" ] && [ "$extensionRegex" == "" ];then
+# CHECK TO SEE IF A VALUE WAS PASSED IN PARAMETER 4 AND, IF SO, ASSIGN TO "extensionRegex"
+if [ "$4" != "" ] && [ "$extensionRegex" == "" ];then
     extensionRegex=$4
 fi
 
@@ -41,20 +30,31 @@ if [ "$extensionRegex" == "" ]; then
 	exit 1
 fi
 
+# CHECK TO SEE IF A VALUE WAS PASSED IN PARAMETER 5 AND, IF SO, ASSIGN TO "pathToExtension"
+if [ "$5" != "" ] && [ "$pathToExtension" == "" ];then
+    pathToExtension=$5
+fi
+
+# Error if variable appName is empty
+if [ "$pathToExtension" == "" ]; then
+	echo "Error:  No value was specified for the pathToExtension variable..."
+	exit 1
+fi
+
 ####################################################################################################
 
 ## Increments for echo at end
 extensionCount=0
 
 ## Echo what we're doing
-echo "Updating extensions with $4..."
+echo "Replacing extensions with a name matching: $extensionRegex  with extension located at $pathToExtension..."
 
 ## Check for home folders under /Users/
 for userHome in /Users/*; do
 	## If the below folder exists
   	if [ -d "${userHome}/Library/Safari/Extensions" ]; then
-  		## Check for the Extension, using the regex as specified at $5 then replace with the Extension at path $4
-    	find "${userHome}/Library/Safari/Extensions" -type f -regex '.*$5.*' -exec cp -v "${4}" "{}" \;
+  		## Check for the Extension, using the regex as specified at $4 then replace with the Extension at path $5
+    	find "${userHome}/Library/Safari/Extensions" -type f -regex '.*'"$extensionRegex"'.*' -exec cp -v "${pathToExtension}" "{}" \;
 		## Increments
 		extensionCount=$((extensionCount+1))
   	fi
